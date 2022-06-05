020795683c0fbb576fd9d3f8e5fb0ce1c5c8a764
Shinra DX <shinra.dx@outlook.com>
2022-06-06 01:45:52 +0800

    修正nonebot.adapters.cqhttp.Message的调用

--- src/plugins/shop.py
+++ src/plugins/shop.py
@@ -8,12 +8,12 @@ send_location = on_command('位置测试')
 @send_location.handle()
 async def _(bot: Bot, event: Event, state: T_State):
     msg = [
-        Message({
+        {
             "type": "location",
             "data": {
                 "lat": "23.70996",
                 "lon": "113.037856",
             },
-        }),
+        },
     ]
-    await send_location.send(msg)
+    await send_location.send(Message(msg))
