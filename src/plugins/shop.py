0cb65821ab69a9c3b91b5490d525e8791bdaa2ac
Shinra DX <shinra.dx@outlook.com>
2022-06-06 01:29:52 +0800

    位置发送测试

---
+++ src/plugins/shop.py
@@ -0,0 +1,19 @@
+from nonebot import on_command
+from nonebot.typing import T_State
+from nonebot.adapters import Event, Bot
+from nonebot.adapters.cqhttp import Message
+
+send_location = on_command('位置测试')
+
+@send_location.handle()
+async def _(bot: Bot, event: Event, state: T_State):
+    msg = [
+        Message({
+            "type": "location",
+            "data": {
+                "lat": "23.70996",
+                "lon": "113.037856",
+            },
+        }),
+    ]
+    await send_location.send(msg)
